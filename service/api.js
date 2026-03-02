e21511060725ede3c3e9627c7531c7fd09c55844
theomark <olivertwistyes@gmail.com>
2026-03-02 14:57:32 +0300

    cleaner url building + timeout

--- service/api.js
+++ service/api.js
@@ -2,8 +2,10 @@ const BASE_URL = 'https://dummyjson.com';
 const USER_FIELDS = 'lastName,firstName,age,gender,phone,email,address';
 const USER_DETAILS = 'lastName,firstName,age,gender,phone,email,address,height,weight,image';
 
-const LIMIT = 30;
+export const MAX_ROWS = 30;
+const TIMEOUT = 10_000;
 
+// if field names change on the server, only constants and this mapper need to change
 const userMapper = (user) => {
     return {
         id: user.id,
@@ -12,33 +14,65 @@ const userMapper = (user) => {
         age: user.age,
         gender: user.gender,
         phone: user.phone,
-        email: user.email,
-        country: user.address?.country || "",
-        city: user.address?.city || "",
-
+        email: user.email
     }
 }
 
-export async function fetchUsersData(page = 0, sortBy, order) {
-    let url = `${BASE_URL}/users?limit=${LIMIT}&skip=${page * LIMIT}&select=${USER_FIELDS}`;
-    if (sortBy && order) url += `&sortBy=${sortBy}&order=${order}`;
+async function fetchWithTimeout(url, options = {}, timeout = TIMEOUT) {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const timer = setTimeout(() => {
+        controller.abort();
+    }, timeout);
 
     try {
-        const res = await fetch(url);
+        const res = await fetch(url, { ...options, signal }); // options in case they are needed in the future
+        if (!res.ok) throw new Error(`HTTP error, status: ${res.status}`);
         const data = await res.json();
-        return {users: data.users.map(user => ({
-            id: user.id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            age: user.age,
-            gender: user.gender,
-            phone: user.phone,
-            email: user.email,
-            country: user.address?.country || "",
-            city: user.address?.city || ""
-        })),
+        return data;
+    } catch (e) {
+        if (e.name === 'AbortError') {
+            throw new Error('Request timed out');
+        }
+        throw e;
+    } finally {
+        clearTimeout(timer);
+    }
+}
+
+export async function fetchUsersData(page = 0, sortBy, order, filterField, filterValue) {
+    const url = new URL(`${BASE_URL}/users${filterField && filterValue ? '/filter' : ''}`);
+
+    const params = new URLSearchParams();
+
+    if (filterField && filterValue) {
+        params.append("key", filterField);
+        params.append("value", filterValue);
+    }
+
+    params.append("limit", MAX_ROWS);
+    params.append("skip", page * MAX_ROWS);
+    params.append("select", USER_FIELDS);
+
+    if (sortBy && order) {
+        params.append("sortBy", sortBy);
+        params.append("order", order === "ascending" ? "asc" : "desc");
+    }
+
+    url.search = params.toString();
+
+    try {
+        const data = await fetchWithTimeout(url);
+        return {
+            users: data.users.map(user => ({
+                ...userMapper(user),
+                country: user.address?.country || "",
+                city: user.address?.city || ""
+            })),
             total: data.total,
-            skip: data.skip};
+            skip: data.skip
+        };
     } catch (e) {
         console.log(e);
         return null;
@@ -50,13 +84,13 @@ export async function fetchSingleUser(id) {
     let url = `${BASE_URL}/users/${id}?select=${USER_DETAILS}`;
 
     try {
-        const res = await fetch(url);
-        const data = await res.json();
+        const data = await fetchWithTimeout(url);
         const userData = {
             ...userMapper(data),
             height: data.height,
             weight: data.weight,
-            avatar: data.image
+            avatar: data.image,
+            address: data.address
         }
         return userData;
     } catch (e) {
